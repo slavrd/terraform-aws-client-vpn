e7c00497f07ab1e1eb367b1b07715b70461a9084
Slav Donchev <slav.donchev@gmail.com>
2019-11-20 14:43:30 +0200

    fix formatting

--- output.tf
+++ output.tf
@@ -1,7 +1,7 @@
 output "aws_ec2_client_vpn_endpoint_id" {
-    value =  aws_ec2_client_vpn_endpoint.vpn_endpoint.id
+  value = aws_ec2_client_vpn_endpoint.vpn_endpoint.id
 }
 
 output "aws_ec2_client_vpn_endpoint_address" {
-    value =  aws_ec2_client_vpn_endpoint.vpn_endpoint.dns_name
+  value = aws_ec2_client_vpn_endpoint.vpn_endpoint.dns_name
 }
